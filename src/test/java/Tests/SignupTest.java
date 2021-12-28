e5ec30559578957b01e7a22d39074eae10ddb59c
aman-sharma98 <sharmaaman1998@hotmail.com>
2021-12-28 18:02:03 +0000

    Backup

---
+++ src/test/java/Tests/SignupTest.java
@@ -0,0 +1,39 @@
+package Tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import Pages.LoginPage;
+import Pages.SignupPage;
+
+
+public class SignupTest extends Baseclass {
+
+	
+	@Parameters({"Email","CorrectPassword","FirstName","LastName","Age","Address"})
+	@Test
+	public void signupTest(String EmailVal,String PasswordVal,String FirstNameVal,String LastNameVal,String AgeVal,String AddressVal) {
+		
+		test = report.startTest("Sign Up Test");
+		SignupPage Signup = new SignupPage();
+		Signup.signup(EmailVal, PasswordVal, FirstNameVal, LastNameVal, AgeVal, AddressVal);
+		Signup.ValidateSignup();
+		report.endTest(test);
+		Assert.assertTrue(true);
+		
+	}
+	
+	@Parameters({"WrongEmail","CorrectPassword","FirstName","LastName","Age","Address"})
+	@Test
+	public void NegativeSignup(String EmailVal,String PasswordVal,String FirstNameVal,String LastNameVal,String AgeVal,String AddressVal) {
+		
+		System.out.println(EmailVal);
+		test = report.startTest("Sign Up Test");
+		SignupPage Signup = new SignupPage();
+		Signup.signup(EmailVal, PasswordVal, FirstNameVal, LastNameVal, AgeVal, AddressVal);
+		Signup.ErrorCheckS();
+		report.endTest(test);
+		Assert.assertTrue(true);
+	}
+}
